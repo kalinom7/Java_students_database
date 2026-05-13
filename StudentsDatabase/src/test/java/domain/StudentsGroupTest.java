db3b466e4a4514b027f813e3c45037c0c372bc2b
48572 <48572@192.168.1.59>
2026-05-13 18:48:51 +0200

    init StudentGroupTest

---
+++ StudentsDatabase/src/test/java/domain/StudentsGroupTest.java
@@ -0,0 +1,14 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudentsGroupTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
